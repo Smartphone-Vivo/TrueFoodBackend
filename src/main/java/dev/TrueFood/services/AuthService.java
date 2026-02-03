6e872afc79b80b49acfe0b30d5cee1e0a0df4a0e
Smartphone-Vivo <aleksejpopov268@gmail.com>
2026-02-03 16:52:19 +0300

    redis

--- src/main/java/dev/TrueFood/services/AuthService.java
+++ src/main/java/dev/TrueFood/services/AuthService.java
@@ -8,16 +8,11 @@ import dev.TrueFood.jwt.JwtResponse;
 import io.jsonwebtoken.Claims;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -26,8 +21,7 @@ public class AuthService {
     private final BaseUserService baseUserService;
     private final JwtProvider jwtProvider;
 
-    private final Map<String, String> refreshStorage = new ConcurrentHashMap<>();
-
+    private final RedisTemplate<String, String> redisTemplate;
 
     public JwtResponse login(SignInRequest signInRequest) {
         final BaseUser baseUser = baseUserService.getByEmail(signInRequest.getEmail())
@@ -40,7 +34,7 @@ public class AuthService {
         if(Objects.equals(encodedPassword, signInRequestPassword) && baseUser.isEnable()){
             final String accessToken = jwtProvider.generateAccessToken(baseUser);
             final String refreshToken = jwtProvider.generateRefreshToken(baseUser);
-            refreshStorage.put(baseUser.getEmail(), refreshToken);
+            redisTemplate.opsForValue().set(baseUser.getEmail(), refreshToken);
             return new JwtResponse(accessToken, refreshToken);
         }else{
             throw new NotFoundException("wrong password");
@@ -52,35 +46,18 @@ public class AuthService {
         if (jwtProvider.validateRefreshToken(refreshToken)) {
             final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
             final String login = claims.getSubject();
-            final String saveRefreshToken = refreshStorage.get(login);
+            final String saveRefreshToken = redisTemplate.opsForValue().get(login);
             if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
                 final BaseUser user = baseUserService.getByEmail(login)
                         .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
                 final String accessToken = jwtProvider.generateAccessToken(user);
                 final String newRefreshToken = jwtProvider.generateRefreshToken(user);
-                refreshStorage.put(user.getEmail(), newRefreshToken);
+                redisTemplate.opsForValue().set(user.getEmail(), refreshToken);
                 return new JwtResponse(accessToken, newRefreshToken);
             }
         }
         return new JwtResponse(null, null);
     }
-
-//    public JwtResponse refresh(@NonNull String refreshToken) {
-//        if (jwtProvider.validateRefreshToken(refreshToken)) {
-//            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
-//            final String login = claims.getSubject();
-//            final String saveRefreshToken = refreshStorage.get(login);
-//            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
-//                final BaseUser user = baseUserService.getByEmail(login)
-//                        .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
-//                final String accessToken = jwtProvider.generateAccessToken(user);
-//                final String newRefreshToken = jwtProvider.generateRefreshToken(user);
-//                refreshStorage.put(user.getEmail(), newRefreshToken);
-//                return new JwtResponse(accessToken, newRefreshToken);
-//            }
-//        }
-//        throw new BadCredentialsException("Невалидный JWT токен");
-//    }
 
 
 }
